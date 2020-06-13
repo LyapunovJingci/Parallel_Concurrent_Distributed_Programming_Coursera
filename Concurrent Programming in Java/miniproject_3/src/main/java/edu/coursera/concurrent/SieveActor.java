dc7eccff656d4212e4481558ee4d876650a3c18e
jingci <jingcikeili@gmail.com>
2020-06-13 13:46:45 -0400

    update week3

--- Concurrent Programming in Java/miniproject_3/src/main/java/edu/coursera/concurrent/SieveActor.java
+++ Concurrent Programming in Java/miniproject_3/src/main/java/edu/coursera/concurrent/SieveActor.java
@@ -1,25 +1,41 @@
 package edu.coursera.concurrent;
 
 import edu.rice.pcdp.Actor;
+import edu.rice.pcdp.PCDP;
 
 /**
  * An actor-based implementation of the Sieve of Eratosthenes.
  *
- * TODO Fill in the empty SieveActorActor actor class below and use it from
+ * Fill in the empty SieveActorActor actor class below and use it from
  * countPrimes to determin the number of primes <= limit.
  */
 public final class SieveActor extends Sieve {
     /**
      * {@inheritDoc}
      *
-     * TODO Use the SieveActorActor class to calculate the number of primes <=
+     * Use the SieveActorActor class to calculate the number of primes <=
      * limit in parallel. You might consider how you can model the Sieve of
      * Eratosthenes as a pipeline of actors, each corresponding to a single
      * prime number.
      */
     @Override
     public int countPrimes(final int limit) {
-        throw new UnsupportedOperationException();
+
+        final SieveActorActor sieveActor = new SieveActorActor(2);
+        PCDP.finish(() -> {
+            for (int i = 3; i <= limit; i += 2) {
+                sieveActor.send(i);
+            }
+            sieveActor.send(0);
+        });
+
+        int numPrimes = 0;
+        SieveActorActor loopActor = sieveActor;
+        while (loopActor != null) {
+            numPrimes += loopActor.numLocalPrimes();
+            loopActor = loopActor.nextActor();
+        }
+        return numPrimes;
     }
 
     /**
@@ -27,16 +43,58 @@ public final class SieveActor extends Sieve {
      * parallel.
      */
     public static final class SieveActorActor extends Actor {
-        /**
-         * Process a single message sent to this actor.
-         *
-         * TODO complete this method.
-         *
-         * @param msg Received message
-         */
+        private int MAXNUM = 1000;
+        private int numLocalPrimes;
+        private int[] localPrimes;
+        private SieveActorActor nextActor;
+
+        SieveActorActor(final int localPrime) {
+            this.localPrimes = new int[MAXNUM];
+            this.localPrimes[0] = localPrime;
+            this.numLocalPrimes = 1;
+            this.nextActor = null;
+        }
+
         @Override
         public void process(final Object msg) {
-            throw new UnsupportedOperationException();
+            final int candidate = (Integer) msg;
+            if (candidate <= 0) {
+                if (nextActor != null) {
+                    nextActor.send(msg);
+                }
+                return;
+            } else {
+                final boolean locallyPrime = isLocallyPrime(candidate);
+                if (locallyPrime) {
+                    if (numLocalPrimes < MAXNUM) {
+                        localPrimes[numLocalPrimes] = candidate;
+                        numLocalPrimes+=1;
+                    } else if (nextActor == null) {
+                        nextActor = new SieveActorActor(candidate);
+                    } else {
+                        nextActor.send(msg);
+                    }
+
+                }
+            }
+        }
+
+
+        private boolean isLocallyPrime(int candidate) {
+            for (int i = 0; i < numLocalPrimes; i++) {
+                if (candidate % localPrimes[i] == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int numLocalPrimes() {
+            return numLocalPrimes;
+        }
+
+        public SieveActorActor nextActor() {
+            return nextActor;
         }
     }
 }
